1a42a283e48dfadce748157ddc0e7fc89ec21321
Ariane Joy Nova <arianenova@gmail.com>
2017-01-16 12:09:46 -0500

    4 command args

--- bash.js
+++ bash.js
@@ -1,19 +1,17 @@
 var commands = require('./commands');
 //var userCommand = 'pwd';
+//var userCommand = 'pwd';
 
 process.stdout.write('prompt > ');
-
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
 
-  commands[cmd]();
-
-  // else if(cmd === "date"){
-  //   var date = new Date();
-  //   process.stdout.write(date.toString());
-  // }
-
-    // process.stdout.write('\nprompt > ');
+  var splitIndex=cmd.indexOf(" ");
+  if(splitIndex >= 0){
+    var parameter= cmd.slice(splitIndex+1);
+    cmd=cmd.slice(0, splitIndex);
+  }
+  commands[cmd](parameter);
 
 });
--- commands.js
+++ commands.js
@@ -17,7 +17,85 @@ module.exports = {
           files.forEach(function(file) {
           process.stdout.write(file.toString() + "\n");
           })
-          process.stdout.write("prompt > ");
-        });}
+          process.stdout.write("\nprompt > ");
+        });
+      },
+  echo: function(str){
+        process.stdout.write(str);
+        process.stdout.write("\nprompt > ");
+      },
+  cat: function(filename){
+        fs.readFile(filename, 'utf8', function(err, data){
+          if(err) throw err;
+          process.stdout.write(data);
+          process.stdout.write("\nprompt > ");
+        });
+      },
+  head: function(filename){
+    fs.readFile(filename, 'utf8', function(err, data){
+      if(err) throw err;
+      var split=data.split("\n");
+      var i=0;
+      var newdata="";
+      while(i < 5 && i < split.length){
+        newdata=newdata.concat(split[i] + "\n");
+        i++;
+      }
+      process.stdout.write(newdata);
+      process.stdout.write("\nprompt > ");
+    });
+  },
+  tail: function(filename){
+    fs.readFile(filename, 'utf8', function(err, data){
+      if(err) throw err;
+      var split=data.split("\n");
+      var i=split.length-5;
+      var newdata="";
+      while(i >= split.length-5 && i < split.length){
+        newdata=newdata.concat(split[i] + "\n");
+        i++;
+      }
+      process.stdout.write(newdata);
+      process.stdout.write("\nprompt > ");
+    });
+  },
+  wc: function(filename){
+    fs.readFile(filename, 'utf8', function(err, data){
+      if(err) throw err;
+      var split=data.split("\n");
+      process.stdout.write(split.length + "");
+      process.stdout.write("\nprompt > ");
+    });
+  },
+  sort: function(filename){
+    fs.readFile(filename, 'utf8', function(err, data){
+      if(err) throw err;
+      var split=data.split("\n");
+      split.sort();
+      process.stdout.write(split.join("\n"));
+      process.stdout.write("\nprompt > ");
+    });
+  },
+  uniq: function(filename){
+    fs.readFile(filename, 'utf8', function(err, data){
+      if(err) throw err;
+      var split=data.split("\n");
+      split.sort();
+      var newdata="";
+      var i=0;
+      while(i < split.length){
+        newdata=newdata.concat(split[i] + "\n");
+        var j=i+1;
+        while(split[i] === split[j]){
+          j++;
+        }
+        i=j;
+      }
+
+
+      process.stdout.write(newdata);
+      process.stdout.write("\nprompt > ");
+    });
+  },
 
 };
